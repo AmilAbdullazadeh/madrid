ba0e4dbea26ed0fdd4e425a538c17839c0d17aa4
Amil A <amil7.abdullayev@gmail.com>
2024-08-22 20:22:47 +0400

    add slide

--- js/main.js
+++ js/main.js
@@ -1,20 +1,35 @@
-const modal = document.querySelector('.modal')
-const overlay = document.querySelector('.overlay')
-const closeBtn = document.querySelector('.btn-close')
-const openBtn = document.querySelector('.btn-open')
+const prevBtn = document.querySelector('.btn-prev')
+const nextBtn = document.querySelector('.btn-next')
+const slides = document.querySelectorAll('.slide')
 
-const openModal = () => {
-    modal.classList.remove('hidden')
-    overlay.classList.remove('hidden')
-}
+let currentSlide = 0
+let latestSlide = slides.length - 1
 
-const closeModal = () => {
-    modal.classList.add('hidden')
-    overlay.classList.add('hidden')
-}
 
-openBtn.addEventListener('click', openModal)
+nextBtn.addEventListener('click', () => {
+    if(currentSlide === latestSlide){
+        currentSlide = 0
+    } else {
+        currentSlide += 1
+    }
 
-closeBtn.addEventListener('click', closeModal)
+    //100 % slide move
+    slides.forEach((slide, idx) =>{
+        slide.style.transform = 'translateX(' + (100 * (idx - currentSlide)) + '%)'
+    })
+})
 
-overlay.addEventListener('click', closeModal)
+
+prevBtn.addEventListener('click', () => {
+    if (currentSlide === 0){
+        currentSlide = latestSlide
+    } else {
+        currentSlide -= 1
+    }
+
+    //-100 % slide move
+    slides.forEach((slide, idx) => {
+        slide.style.transform = 'translateX(' + (100 * (idx - currentSlide)) + '%)'
+    })
+})
+
