9c18fde74cc4b038885ced387843b3313e4bdd27
Amil A <amil7.abdullayev@gmail.com>
2024-08-29 20:02:26 +0400

    skillsbar

--- js/main.js
+++ js/main.js
@@ -1,9 +1,34 @@
-let menuBar = document.getElementById('menu-bar');
-let nav = document.getElementById('nav');
-let menuBg = document.getElementById('menu-bg');
+// Select all elements with the class "progressbar"
+const skills = document.querySelectorAll('.progressbar');
+skills.forEach(skill => {
+    console.log(skill.id);
+})
+// ['html', 'css', 'javascript']
 
-function toggleMenu() {
-    menuBar.classList.toggle('change');
-    nav.classList.toggle('change');
-    menuBg.classList.toggle('change-bg');
-}
+// Define a list of skills with their respective percentages
+const skillList = [
+    { name: 'html', percentage: '90%' },
+    { name: 'css', percentage: '80%' },
+    { name: 'javascript', percentage: '70%' },
+    { name: 'react', percentage: '60%' }
+]
+
+    function widthBar() {
+        // Iterate over each skill element
+        skills.forEach((skill, index) => {
+            // Iterate over each skill in the skill list
+            skillList.forEach(item => {
+                // Check if the element's id matches the skill's name
+                if (("progress-" + item.name) === skill.id) {
+                    // Set a timeout to stagger the animation of each progress bar
+                    setTimeout(() => {
+                        // Set the width of the progress bar to the skill's percentage
+                        skill.style.width = item.percentage;
+                    }, 500 * index);
+                }
+            });
+        });
+    }
+
+    widthBar()
+
