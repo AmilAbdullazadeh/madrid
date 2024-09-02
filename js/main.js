d413683bbb73667df18687f6c83d73957b0014a7
Amil A <amil7.abdullayev@gmail.com>
2024-09-02 19:30:00 +0400

    task 1

--- js/main.js
+++ js/main.js
@@ -1,34 +1,13 @@
-// Select all elements with the class "progressbar"
-const skills = document.querySelectorAll('.progressbar');
-skills.forEach(skill => {
-    console.log(skill.id);
-})
-// ['html', 'css', 'javascript']
+// 5. Create Buttons from an Array and Alert their Values
+const buttonNames = ['Button 1', 'Button 2', 'Button 3'];
+const buttonContainer = document.querySelector('#button-container');
 
-// Define a list of skills with their respective percentages
-const skillList = [
-    { name: 'html', percentage: '90%' },
-    { name: 'css', percentage: '80%' },
-    { name: 'javascript', percentage: '70%' },
-    { name: 'react', percentage: '60%' }
-]
-
-    function widthBar() {
-        // Iterate over each skill element
-        skills.forEach((skill, index) => {
-            // Iterate over each skill in the skill list
-            skillList.forEach(item => {
-                // Check if the element's id matches the skill's name
-                if (("progress-" + item.name) === skill.id) {
-                    // Set a timeout to stagger the animation of each progress bar
-                    setTimeout(() => {
-                        // Set the width of the progress bar to the skill's percentage
-                        skill.style.width = item.percentage;
-                    }, 500 * index);
-                }
-            });
-        });
-    }
-
-    widthBar()
-
+buttonNames.forEach((buttonName) => {
+    debugger
+    const buttonElement = document.createElement('button');
+    buttonElement.textContent = buttonName;
+    buttonElement.addEventListener('click', () => {
+        alert(buttonName);
+    });
+    buttonContainer.appendChild(buttonElement);
+});
