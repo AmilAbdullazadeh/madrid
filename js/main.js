54d43245de2c94d0f0f9277b9da6f539cf33cddf
Amil A <amil7.abdullayev@gmail.com>
2024-09-02 20:09:26 +0400

    task 2

--- js/main.js
+++ js/main.js
@@ -1,13 +1,38 @@
-// 5. Create Buttons from an Array and Alert their Values
-const buttonNames = ['Button 1', 'Button 2', 'Button 3'];
-const buttonContainer = document.querySelector('#button-container');
+// Countdown Timer
+// Task: Create a countdown timer that counts down from a specified number of seconds.
+// When the countdown reaches zero, show a message like "Time's up!".
 
-buttonNames.forEach((buttonName) => {
-    debugger
-    const buttonElement = document.createElement('button');
-    buttonElement.textContent = buttonName;
-    buttonElement.addEventListener('click', () => {
-        alert(buttonName);
-    });
-    buttonContainer.appendChild(buttonElement);
-});
+const startBtn = document.querySelector("#start-btn");
+const timerDisplay = document.querySelector("#timer-display");
+
+const secondsFromClient = prompt("Enter the number of seconds for the countdown timer: ");
+
+let countdown = null;
+
+function startTimer() {
+    clearInterval(countdown);
+    let timeLeft = parseInt(secondsFromClient)
+
+
+    function updateTimer() {
+        timerDisplay.textContent = convertSecondsToMinutes(timeLeft);
+
+        if (timeLeft > 0) {
+            timeLeft--;
+        } else {
+            clearInterval(countdown);
+            timerDisplay.textContent = "Time's up!";
+        }
+    }
+
+    countdown = setInterval(updateTimer, 1000)
+    updateTimer()
+}
+
+function convertSecondsToMinutes(secondsTimeLeft) {
+    let minutes = Math.floor(secondsTimeLeft / 60);
+    let seconds = secondsTimeLeft % 60;
+    return `Time left: ${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
+}
+
+startBtn.addEventListener("click", startTimer)
