297384c203d91b5a7569a329c53bc2f18adaff57
Amil A <amil7.abdullayev@gmail.com>
2024-09-10 20:26:55 +0400

    free api

--- js/main.js
+++ js/main.js
@@ -1,49 +1,29 @@
-import {
-    checkLength,
-    checkLowercase,
-    checkNumbers,
-    checkRepeatedCharacters,
-    checkSpecialCharacters,
-    checkUppercase
-} from "./helper";
-
-const strengthMeter = document.getElementById('strength-meter');
-const passwordInput = document.getElementById('password-input');
-const reasonsContainer = document.getElementById('reasons');
-
-passwordInput.addEventListener('input', updateStrengthMeter);
-// updateStrengthMeter()
-
-function updateStrengthMeter() {
-    let strength = 100
-    const password = passwordInput.value
-
-    const weaknesses = calculatePasswordStrength(password)
-    reasonsContainer.innerHTML = ''
-
-    weaknesses.forEach(weakness => {
-        if (weakness) {
-            strength = strength - weakness.deduction
-
-            const messageElement = document.createElement('div')
-            messageElement.innerText = weakness.message
-            reasonsContainer.appendChild(messageElement)
-        }
-    })
-
-    strengthMeter.style.setProperty('--strength', strength)
+const config = {
+    name: 'NASA API',
+    baseUrl: 'https://api.nasa.gov/planetary/apod?api_key=',
+    apiKey: '84pDsFJmvwh47VlR9zAdJhQinXKT2jgvwmcE6Tr'
 }
 
-function calculatePasswordStrength(password) {
-    const weaknesses = []
-    weaknesses.push(checkLength(password))
-    weaknesses.push(checkUppercase(password))
-    weaknesses.push(checkLowercase(password))
-    weaknesses.push(checkNumbers(password))
-    weaknesses.push(checkSpecialCharacters(password))
-    weaknesses.push(checkRepeatedCharacters(password))
+const title = document.querySelector("#title")
+const date = document.querySelector("#date")
+const picture = document.querySelector("#picture");
+const explanation = document.querySelector('#explanation')
 
-    return weaknesses
+async function getNASAData() {
+    try {
+        const response = await fetch(`${config.baseUrl}${config.apiKey}`)
+        const data = await response.json()
+        displayNASAData(data)
+    } catch (error) {
+        alert('An error occurred while fetching data from the NASA API')
+    }
 }
 
+getNASAData()
 
+function displayNASAData(data) {
+    title.textContent = data.title
+    date.textContent = data.date
+    picture.src = data.url
+    explanation.textContent = data.explanation
+}
