6ce9045afb73e4e6f6cf514b368fa445ea356783
Amil A <amil7.abdullayev@gmail.com>
2024-09-12 19:54:35 +0400

    JQUERY, AJAX

--- js/main.js
+++ js/main.js
@@ -1,34 +1,26 @@
-const recipeList = document.querySelector('#recipe-list');
-
 const config = {
-    apiKey: '275d58779ccf4e22af03e792e8819fff',
-    baseUrl: 'https://api.spoonacular.com/recipes/random?number=50&apiKey='
+    baseUrl: 'https://geek-jokes.sameerkumar.website/api?format=json'
 }
 
-const fetchRecipes = async () => {
-    const response = await fetch(`${config.baseUrl}${config.apiKey}`);
-    const data = await response.json();
+$(document).ready(function() {
+    fetchJoke()
+})
 
-    displayRecipes(data.recipes);
+function fetchJoke() {
+    $.ajax({
+        url: config.baseUrl,
+        type: 'GET',
+        success: (data) => onSuccess(data),
+        error: (data) => onError(data)
+    })
 }
 
-fetchRecipes()
-
-const displayRecipes = (recipes) => {
-    recipes.forEach((recipe) => {
-        const recipeItem = document.createElement('li');
-        recipeItem.classList.add('recipe-item');
-        recipeItem.innerHTML = `
-            <img
-                src="${recipe.image}"
-                alt="${recipe.title}"
-            />
-            <h2>${recipe.title}</h2>
-            <p>
-                <strong>Ingredients:</strong> ${recipe.extendedIngredients.map((ingredient) => ingredient.originalName).join(', ')}
-            </p>
-            <a href="" target="_blank">View Recipe</a>
-        `;
-        recipeList.appendChild(recipeItem);
-    });
+function onSuccess(data) {
+    $('#joke').text(data.joke)
 }
+
+function onError(err) {
+    console.error(err)
+}
+
+$('#btn').click(fetchJoke)
